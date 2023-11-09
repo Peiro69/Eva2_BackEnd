a320d2197615a2ec1a2be8db597fe82cd12d4b6f
Peiro69 <cayunaopedro3@gmail.com>
2023-11-08 23:06:29 -0300

    SELECT LOGRADO
    se pudo generar el select dentro del formulario y solo faltan las validaciones

--- BackEnd/proyectoDirector/appDirector/models.py
+++ BackEnd/proyectoDirector/appDirector/models.py
@@ -3,11 +3,15 @@ from django.db import models
 
 # Create your models here.
 
+OPCIONES = [('Masculino','Masculino'), ('Femenino','Femenino'),('No Aplica','Prefiero no decirlo')]
+
+
+
 class Director (models.Model):
  
     nombre = models.CharField(max_length=50)
-    edad = models.CharField(max_length=50)
-    genero = models.CharField(max_length=50)
+    edad = models.IntegerField()
+    genero = models.CharField(max_length=20,choices=OPCIONES)
     nacionalidad = models.CharField(max_length=50)
     premios = models.IntegerField()
     cantidad_peliculas = models.IntegerField()
